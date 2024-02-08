da3c3b1a9f3da6dff2f9d04549461ea9123e37fa
fangqiank <fangqiank@yahoo.com>
2024-02-08 08:05:00 -0700

    remove drizzle error

--- src/graphql/drizzle-orm-demo/src/routes/users/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { createUser, getUserPosts } from './handlers';
-
-export const router = Router();
-
-router.get('/:id/posts', getUserPosts);
-router.post('/', createUser);
